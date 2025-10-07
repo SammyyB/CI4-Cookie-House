8bc7bff8a61d056ac67f8cc75d5d9f88ffbb190a
SammyyB <baccaysheryll@gmail.com>
2025-10-08 02:09:48 +0800

    Feat(SignUpPage): created the signup page branch
    - added routes.php
    - added Users.php
    - finished the sign-up-page.php
    - added both the logo and styles under assets

---
+++ backend/app/Controllers/Users.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+    public function signup(): string
+    {
+        return view('user/signup_page');
+    }
+}
