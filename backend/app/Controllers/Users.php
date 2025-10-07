4b6796b7e04f1d1df88ee40dcb858e0ae0b99f54
SammyyB <baccaysheryll@gmail.com>
2025-10-08 01:50:05 +0800

    Feat(LoginPage): created the login page branch
    - added the routes, users
    - created the styles.css
    - finished and polished the login_page.php

---
+++ backend/app/Controllers/Users.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Users extends BaseController
+{
+    public function login(): string
+    {
+        return view('user/login_page');
+    }
+}
