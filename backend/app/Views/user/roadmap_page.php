b23b7a96e23e168ada8ab2dc5853e2a53f1d0f10
SammyyB <baccaysheryll@gmail.com>
2025-10-09 02:59:33 +0800

    feat(frontend): created the roadmap branch - added image assets - added styles.css - backend: routes and users - created the roadmap_page.php - refractor: added the fragmentation

---
+++ backend/app/Views/user/roadmap_page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body class="roadmap-page">
+
+    <?= view('components/header') ?>
+
+    <?= view('components/navbar') ?>
+
+    <main class="roadmap-main">
+        <h1>System Road Map</h1>
+        <?= view('components/cards/roadmapcards') ?>
+    </main>
+
+
+    <?= view('components/footer') ?>
+</body>
+
+</html>
