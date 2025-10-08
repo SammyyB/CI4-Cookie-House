35a491ccffc9716b5717a4e841aeeae5577b5162
SammyyB <baccaysheryll@gmail.com>
2025-10-09 03:32:24 +0800

    fix(frontend): syntax error

--- backend/app/Views/user/roadmap_page.php
+++ backend/app/Views/user/roadmap_page.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<?= view('components/head/head_roadmap') ?
+<?= view('components/head/head_roadmap') ?>
 
 <body class="roadmap-page">
 
+    <?= view('components/header') ?>
 
-<?= view('components/header') ?>
-
-<?= view('components/navbar') ?>
+    <?= view('components/navbar') ?>
 
     <main class="roadmap-main">
         <h1>System Road Map</h1>
