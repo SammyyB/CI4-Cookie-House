2f0ba2546c4cde009131ce83b706a90803f2487a
SammyyB <baccaysheryll@gmail.com>
2025-10-09 12:53:42 +0800

    refractor(frontend): fixed the fragmentation

--- backend/app/Views/components/navbar.php
+++
@@ -1,7 +0,0 @@
-    <nav class="roadmap-nav">
-        <a href="/">Home</a>
-        <a href="/login">Login</a>
-        <a href="/signup">Sign Up</a>
-        <a href="/moodboard">Mood Board</a>
-        <a href="/roadmap">Road Map</a>
-    </nav>
