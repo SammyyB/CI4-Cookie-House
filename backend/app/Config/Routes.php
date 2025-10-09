6da1c6b08bdb8838fda893c3afa458e31bce2e79
SammyyB <baccaysheryll@gmail.com>
2025-10-09 19:16:41 +0800

    refractor(roadmap): fixed the routes and users controller

--- backend/app/Config/Routes.php
+++ backend/app/Config/Routes.php
@@ -2,8 +2,4 @@
 
 use CodeIgniter\Router\RouteCollection;
 
-$routes->get('/', 'Users::index');
-$routes->get('/login', 'Users::login');
-$routes->get('/signup', 'Users::signup');
-$routes->get('/moodboard', 'Users::moodboard');
 $routes->get('/roadmap', 'Users::roadmap');
--- backend/app/Controllers/Users.php
+++ backend/app/Controllers/Users.php
@@ -7,26 +7,6 @@ use App\Controllers\BaseController;
 class Users extends BaseController
 {
 
-    public function index(): string
-    {
-        return view('user/landing_page');
-    }
-
-    public function login(): string
-    {
-        return view('user/login_page');
-    }
-
-    public function signup(): string
-    {
-        return view('user/signup_page');
-    }
-
-    public function moodboard(): string
-    {
-        return view('user/moodboard_page');
-    }
-
     public function roadmap(): string
     {
         return view('user/roadmap_page');
