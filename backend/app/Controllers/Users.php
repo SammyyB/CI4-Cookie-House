2a23abe8aab35077646e8c2643f2e210b9515705
SammyyB <baccaysheryll@gmail.com>
2025-10-09 19:15:51 +0800

    refractor(loginpage): fix the routes and users controller

--- backend/app/Controllers/Users.php
+++ backend/app/Controllers/Users.php
@@ -11,24 +11,4 @@ class Users extends BaseController
     {
         return view('user/landing_page');
     }
-
-    public function login(): string
-    {
-        return view('user/login_page');
-    }
-
-    public function signup(): string
-    {
-        return view('user/signup_page');
-    }
-
-    public function moodboard(): string
-    {
-        return view('user/moodboard_page');
-    }
-
-    public function roadmap(): string
-    {
-        return view('user/roadmap_page');
-    }
 }
